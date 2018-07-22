583ca9d91dffcdfb65da3e63594c89f9cafd58ef
Anwesh43 <anweshthecool0@gmail.com>
2018-07-22 09:33:53 +0530

    importing libraries and defining number of nodes

--- trnview/src/main/java/com/anwesh/uiprojects/trnview/TRNView.kt
+++ trnview/src/main/java/com/anwesh/uiprojects/trnview/TRNView.kt
@@ -3,3 +3,12 @@ package com.anwesh.uiprojects.trnview
 /**
  * Created by anweshmishra on 22/07/18.
  */
+
+import android.view.View
+import android.content.Context
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+
+val nodes : Int = 5 
