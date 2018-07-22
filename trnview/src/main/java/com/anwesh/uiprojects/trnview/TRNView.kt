de0881c179402f92a7c1a29d3e527d0c497d31cd
Anwesh43 <anweshthecool0@gmail.com>
2018-07-22 09:37:37 +0530

    adding TRNView class

--- trnview/src/main/java/com/anwesh/uiprojects/trnview/TRNView.kt
+++ trnview/src/main/java/com/anwesh/uiprojects/trnview/TRNView.kt
@@ -11,4 +11,22 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Path
 
-val nodes : Int = 5 
+val nodes : Int = 5
+
+class TRNView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent): Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
