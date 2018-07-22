b2941357d431448df9184e43bf5267273907dbb2
Anwesh43 <anweshthecool0@gmail.com>
2018-07-22 09:46:24 +0530

    adding State class

--- trnview/src/main/java/com/anwesh/uiprojects/trnview/TRNView.kt
+++ trnview/src/main/java/com/anwesh/uiprojects/trnview/TRNView.kt
@@ -29,4 +29,24 @@ class TRNView(ctx : Context) : View(ctx) {
         }
         return true
     }
+
+    data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+
+        fun update(stopcb : (Float) -> Unit) {
+            scale += dir * 0.1f
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                stopcb(prevScale)
+            }
+        }
+
+        fun startUpdating(startcb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1 - 2 * prevScale
+                startcb()
+            }
+        }
+    }
 }
