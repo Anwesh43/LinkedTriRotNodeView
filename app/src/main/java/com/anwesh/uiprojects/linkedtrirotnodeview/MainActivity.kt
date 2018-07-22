99dd468d93d138e052fcf6976aacd42504fbb573
Anwesh43 <anweshthecool0@gmail.com>
2018-07-22 10:12:33 +0530

    using trnview  in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedtrirotnodeview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtrirotnodeview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.anwesh.uiprojects.linkedtrirotnodeview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.trnview.TRNView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        TRNView.create(this)
     }
 }
