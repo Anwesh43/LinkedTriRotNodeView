202b4e0e49b9186501a08029550db5c0b39c8a46
Anwesh43 <anweshthecool0@gmail.com>
2018-07-22 10:04:38 +0530

    adding TRNNode class

--- trnview/src/main/java/com/anwesh/uiprojects/trnview/TRNView.kt
+++ trnview/src/main/java/com/anwesh/uiprojects/trnview/TRNView.kt
@@ -145,4 +145,28 @@ class TRNView(ctx : Context) : View(ctx) {
         }
 
     }
+
+    data class LinkedTRNNode(var i : Int) {
+
+        private var curr : TRNNode = TRNNode(0)
+
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(stopcb : (Int, Float) -> Unit)  {
+            curr.update {i, scale ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                stopcb(i, scale)
+            }
+        }
+
+        fun startUpdating(startcb : () -> Unit) {
+            curr.startUpdating(startcb)
+        }
+    }
 }
