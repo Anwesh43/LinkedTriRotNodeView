2b781288de76a26bc2a24083909412f3e4b28b2a
Anwesh43 <anweshthecool0@gmail.com>
2018-07-22 18:51:41 +0530

    adding OnAnimationListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedtrirotnodeview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtrirotnodeview/MainActivity.kt
@@ -3,14 +3,24 @@ package com.anwesh.uiprojects.linkedtrirotnodeview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.trnview.TRNView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        TRNView.create(this)
+        val view : TRNView = TRNView.create(this)
         fullScreen()
+        view.addOnAnimationListener({
+            showToast("animation number ${it + 1} is complete")
+        }, {
+            showToast("animation numnber ${it + 1} is reset")
+        })
+    }
+
+    private fun showToast(text : String) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
     }
 }
 
